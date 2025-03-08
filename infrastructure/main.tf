590eb3264b559053efd1e0478c77145432605521
Aliasgar <aliasgar@mac.home>
2025-03-07 22:34:25 -0500

    bad formatting

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -23,5 +23,5 @@ resource "azurerm_storage_account" "test_storage" {
   resource_group_name      = azurerm_resource_group.test_rg.name
   location                 = azurerm_resource_group.test_rg.location
   account_tier             = "Standard"
-  account_replication_type = "LRS"
+account_replication_type = "LRS"
 }
